99cadb6fe1aa059c47f8cec22e2b2123d1f23e1a
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-04 08:21:57 +0900

    Updates

---
+++ component---src-pages-page-2-tsx-cde34fe1f420bcf2c5cc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"+xhR":function(e,t,a){"use strict";a.r(t);var n=a("Wbzz"),o=a("q1tI"),l=a.n(o),c=a("9Dj+"),r=a("H8eV");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-cde34fe1f420bcf2c5cc.js.map
