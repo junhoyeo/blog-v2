42ce0f1370448f0a3fb289d2147ed46b56b3ff58
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-05 03:00:38 +0900

    chore: add generated graphql-types.d.ts in .gitignore

--- src/types/graphql-types.d.ts
+++ src/types/graphql-types.d.ts
@@ -1856,6 +1856,8 @@ export type QuerySiteArgs = {
   children?: Maybe<NodeFilterListInput>,
   internal?: Maybe<InternalFilterInput>,
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>,
+  port?: Maybe<IntQueryOperatorInput>,
+  host?: Maybe<StringQueryOperatorInput>,
   pathPrefix?: Maybe<StringQueryOperatorInput>,
   polyfill?: Maybe<BooleanQueryOperatorInput>,
   buildTime?: Maybe<DateQueryOperatorInput>
@@ -1924,6 +1926,8 @@ export type Site = Node & {
   children: Array<Node>,
   internal: Internal,
   siteMetadata?: Maybe<SiteSiteMetadata>,
+  port?: Maybe<Scalars['Int']>,
+  host?: Maybe<Scalars['String']>,
   pathPrefix?: Maybe<Scalars['String']>,
   polyfill?: Maybe<Scalars['Boolean']>,
   buildTime?: Maybe<Scalars['Date']>,
@@ -2056,6 +2060,8 @@ export type SiteFieldsEnum =
   'siteMetadata___description' |
   'siteMetadata___keywords' |
   'siteMetadata___siteUrl' |
+  'port' |
+  'host' |
   'pathPrefix' |
   'polyfill' |
   'buildTime';
@@ -2066,6 +2072,8 @@ export type SiteFilterInput = {
   children?: Maybe<NodeFilterListInput>,
   internal?: Maybe<InternalFilterInput>,
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>,
+  port?: Maybe<IntQueryOperatorInput>,
+  host?: Maybe<StringQueryOperatorInput>,
   pathPrefix?: Maybe<StringQueryOperatorInput>,
   polyfill?: Maybe<BooleanQueryOperatorInput>,
   buildTime?: Maybe<DateQueryOperatorInput>,
